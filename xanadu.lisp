9d14d8462e5438fbc4553e1227a6ba493da65932
complexitycollapse <complexitycollapse@protonmail.com>
2020-10-20 23:58:14 +0100

    Loading and saving files

--- xanadu.lisp
+++ xanadu.lisp
@@ -7,3 +7,22 @@
 
 (defun parse-name (name-string)
   (mapcar #'read-from-string (split-sequence #\. name-string)))
+
+(defparameter repo+ "~/lisp/xanadu/test-repo")
+
+(defun name-to-path (name) (format nil "~A/~{~A~^/~}" repo+ name))
+
+(defun ensure-directories-for-name (name)
+  (ensure-directories-exist (name-to-path name)))
+
+(defun load-by-name (name)
+  (let ((file (make-array '(0) :element-type 'character :adjustable T :fill-pointer 0)))
+    (with-open-file (s (name-to-path name))
+      (do ((c (read-char s nil :eof) (read-char s nil :eof)))
+	  ((eq c :eof) file)
+	(vector-push-extend c file)))))
+
+(defun save-by-name (name contents)
+  (ensure-directories-for-name name)
+  (with-open-file (s (name-to-path name) :direction :output :if-exists :supersede)
+    (loop :for c :across contents :do (write-char c s))))
