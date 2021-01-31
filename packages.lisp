e8076e62d5369a71f9625e92a1d963b010e61cf1
complexitycollapse <complexitycollapse@protonmail.com>
2021-01-31 23:37:04 +0000

    Exported NEW-LINK

--- packages.lisp
+++ packages.lisp
@@ -4,4 +4,5 @@
   (:use #:cl #:cl-utilities)
   (:export
    :new-doc :append-text :insert-text :delete-text :move-text :transclude :delete-leaf
-   :import-file :export-text :doc-length :add-link :insert-link :remove-link :make-exe))
+   :import-file :export-text :doc-length :new-link :add-link :insert-link :remove-link
+   :make-exe))
